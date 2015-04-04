24d13e8327bcd2e1b95956511326884f093396fa
xiaohaizhe <greyrobot@163.com>
2015-04-04 23:52:26 +0800

    add TestClob

---
+++ src/com/novahome/data/pojo/TestClob.java
@@ -0,0 +1,51 @@
+package com.novahome.data.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class TestClob {
+
+	
+	private String id;
+	
+	
+
+	private String name;
+	
+	private String content;
+
+	@Id
+	@Column(length = 32)
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
