27cb037661c23db4df98f60e266a71779710f1e8
xiaohaizhe <greyrobot@163.com>
2015-04-04 23:53:05 +0800

    test

---
+++ src/com/novahome/data/dao/TestClobDao.java
@@ -0,0 +1,39 @@
+package com.novahome.data.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+import com.novahome.data.pojo.TestClob;
+
+@Component("testClobDao")
+public class TestClobDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	public String saveTestClob(TestClob testClob) {
+		Session s = sessionFactory.getCurrentSession();
+		return s.save(testClob).toString();
+	}
+	
+	public void updateText(TestClob text) {
+		Session s = sessionFactory.getCurrentSession();
+		s.update(text);
+	}
+
+	public TestClob getTextById (String name) {
+		return (TestClob)sessionFactory.getCurrentSession().get(TestClob.class,name);
+	}
+	
+	public long deleteTextById (String name) {
+		Query query = sessionFactory.getCurrentSession().createQuery(
+		"delete from Text where name = :name");
+		query.setParameter("name", name);
+		
+		return query.executeUpdate();
+	}
+}
+
+
