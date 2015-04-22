ba72365530860940701775a587a68a3a29bef033
xiaohaizhe <greyrobot@163.com>
2015-04-22 17:12:39 +0800

    增加媒体中心静态页面

--- WebContent/jslib/16sucai.js
+++ WebContent/jslib/16sucai.js
@@ -4,6 +4,7 @@ $(document).ready(function()
 	//slides the element with class "menu_body" when paragraph with class "menu_head" is clicked 
 	$("#firstpane a.menu_head").click(function()
     {
+		 $(".menu_Left").removeClass("menu_active");
 		$(this).css({backgroundImage:"url(images/cdhli2.jpg)",color:"#fff"}).next("div.menu_body").slideToggle(300).siblings("div.menu_body").slideUp("slow");
        	$(this).siblings().css({backgroundImage:"",color:"#222222"});
 	});
