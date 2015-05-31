fb330feafc7fad32134b802d2059cf06f71bc999
JZW <371899560@qq.com>
2015-05-31 16:57:17 +0800

    修改部分翻译，静态页面

---
+++ WebContent/eng/js/constants.js
@@ -0,0 +1,13 @@
+//现场服务类别
+var SCENE_IMAGE_BASE = "../resources/xcfwImages/";
+var CON_SCENE_TYPE = [
+                      {name:"Barstool",image:"ydcp.jpg",info:["10 days in advance booking Price: 40.00 "," site lease Price: 60.00 ","  deposit: 100.00"]},
+                      {name:"Armchair",image:"ydcp.jpg",info:["10 days in advance booking Price: 40.00 "," site lease Price: 60.00 "," deposit: 100.00"]},
+                      {name:"Folding Chair",image:"ydcp.jpg",info:["10 days in advance booking Price: 40.00 "," site lease Price: 60.00 "," deposit: 100.00"]}
+                      ];
+
+//货运物流
+var CON_TRANS_TYPE = [
+                      {name:"Site forklift",info:["Unit：3-ton forklift","Price:100Yuan/Hour","Note: less than 2 hours"]},
+                      {name:"Truck Crane",info:["Unit：3-ton forklift","Price::100Yuan/Hour","Note: less than 2 hours"]}
+                      ];
