9399c25bcc92f79571a97f62ecd6aa22ad72bc38
xiaohaizhe <greyrobot@163.com>
2015-07-30 15:19:51 +0800

    增加证件二维码功能

--- src/com/novahome/utils/BarcodeUtils.java
+++ src/com/novahome/utils/BarcodeUtils.java
@@ -9,9 +9,6 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.novahome.commonservice.Constants;
 
-
-
-
 public class BarcodeUtils {
 
 	private static final Logger logger = Logger.getLogger(BarcodeUtils.class);
@@ -19,6 +16,32 @@ public class BarcodeUtils {
 	{
 		int width = 100;
 		int height = 100;
+		String format = "png";
+		String picName ;
+		try {
+			String nowpath = System.getProperty("user.dir");            
+			String tempdir = nowpath.replace("bin", "webapps");
+			tempdir+="\\"+ ConfigUtils.getPrj(); 
+			String basePath = tempdir + Constants.BARCODE_MID_STR;
+			picName = UUID.randomUUID() + ".png";
+			String fullPath = basePath + picName;
+			BitMatrix bitMatrix = new MultiFormatWriter().encode(content,BarcodeFormat.QR_CODE,width,height);
+			
+			File outputFile = new File(fullPath);
+			logger.info("barcode filename:" + fullPath);
+			MatrixToImageWriter.writeToFile(bitMatrix, format, outputFile);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return picName;
+	}
+	
+	public static String createBadgeBarcode(String content)
+	{
+		int width = 70;
+		int height = 70;
 		String format = "png";
 		String picName ;
 		try {
